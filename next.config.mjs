01bbc370e9d32cac6a271f7b3f1137974cdf9823
Denis <sustain@bk.ru>
2023-01-03 17:01:22 +0300

    start the dance

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true
 }
 
-module.exports = nextConfig
+
+export default nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import '@/public/styles/index.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
